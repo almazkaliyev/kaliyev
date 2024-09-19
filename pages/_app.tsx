cd771cb368436d65b084ea0ef223f0747755b3bf
Almaz Kaliyev <almaz@kaliyev.dev>
2024-09-20 02:58:25 +0500

    Обновил пакеты

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,20 +1,19 @@
-import { AppType } from 'next/dist/shared/lib/utils';
 import Head from 'next/head';
 
 import '@/styles/app.css';
 
-const App: AppType = ({ Component, pageProps }) => (
-  <>
-    <Head>
-      <title>kaliyev.</title>
-      <meta name="description" content="Алмаз Калиев - Frontend разработчик" />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
-    <main className="w-screen full-height p-4 flex justify-content-center items-center">
-      <Component {...pageProps} />
-    </main>
-  </>
-);
-
-export default App;
+export default function App ({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <title>kaliyev.</title>
+        <meta name="description" content="Алмаз Калиев - Frontend разработчик" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className="w-screen full-height p-4 flex justify-content-center items-center">
+        <Component {...pageProps} />
+      </main>
+    </>
+  );
+}
