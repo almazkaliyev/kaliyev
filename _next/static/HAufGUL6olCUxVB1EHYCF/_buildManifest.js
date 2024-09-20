d0c2d8372e45f9edc78f5cf7053be5af04d6b8ac
almazkaliyev <almazkaliyev@users.noreply.github.com>
2024-09-20 20:13:00 +0000

    deploy: 2847d37a536978bce9b6e202ae3632a3b54747f1

--- _next/static/HAufGUL6olCUxVB1EHYCF/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/pages/index-20e8bf205df35d6f.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
